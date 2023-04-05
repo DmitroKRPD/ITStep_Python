eb171b05018bee461647b8f600a00122a734df00
User Office <user@kr.step>
2023-04-05 20:01:41 +0300

    Practic 1

---
+++ Pr01/1.py
@@ -0,0 +1,15 @@
+# # print('Hello world!!!') #виводить на екран
+# # userName = input('Введіть ваше ім\'я: ')
+# # print('    Вітаю Вас,',userName)
+
+# print('             Nothing','will work','unless you do',sep='\n             ')
+# print('             Nothing','will work','unless you do',sep='\n             ')
+# print('             Nothing','will work','unless you do',sep='\n             ')
+# print('             Nothing','will work','unless you do',sep='\n             ')
+# print('             Nothing','will work','unless you do',sep='\n             ')
+
+number = int(input('input number: '))
+a = number // 10
+b = number % 10
+print(a)
+print(b)
